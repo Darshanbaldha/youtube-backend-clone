729413689b0e9b7f68c709b004ad2de55da46444
Darshan <112396567+Darshanbaldha@users.noreply.github.com>
2025-06-22 12:19:06 +0530

    Add Cloudinary integration and multer middleware
    Added environment variables for Cloudinary configuration in .env.sample. Installed and configured 'cloudinary' and 'multer' packages in package.json. Introduced src/utils/cloudinary.js for file uploads to Cloudinary and src/middlewares/multer.middlewares.js for handling file uploads with multer. Fixed a typo in the ApiError class stack property.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+// file upload
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    } else {
+      const response = await cloudinary.uploader
+        .upload(localFilePath, {
+          resource_type: "auto",
+        })
+        .catch((error) => {
+          console.log(`File not Uploded : ${error}`);
+        });
+      console.log(`File uploaded on Cloudinary`, response.url);
+      return response;
+    }
+  } catch (error) {
+    // remove local temporary file from multer when file operation got failed.
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export default uploadOnCloudinary;
